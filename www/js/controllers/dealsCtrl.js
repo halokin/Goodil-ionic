f061410d9a9f0a075819f7705298ab59154a80ab
halokin <halokin@gmail.com>
2016-05-11 10:03:31 +0200

    commit files init

---
+++ www/js/controllers/dealsCtrl.js
@@ -0,0 +1,29 @@
+function dealsCtrl ($scope) {
+	$scope.deals=[
+	{
+	"shop":{
+		"name": "Leclerc",
+		"logo":"https://foodwineandstyle.files.wordpress.com/2015/09/logo-leclerc.jpg",
+		"city":  "La Loupe",
+		"category":{
+			"name":"Alimentaire",
+			"logo":"http://emballages.arts-et-metiers.net/images/logo-alimentaire.png",
+			},
+		},
+	"name": "Offre de Noel",
+	"end_date":"20/12/16",
+	},
+	{
+	"shop":{
+		"name": "Intermarche",
+		"logo":"http://www.hopscotchgroupe.com/assets/uploads/2015/11/logo_leclerc-640x350.jpg",
+		"city":  "La Loupe",
+		"category":{
+			"name":"Alimentaire",
+			"logo":"http://images.google.fr/url?sa=t&rct=j&q=&source=imgres&cd=1&cad=rja&uact=8&ved=0ahUKEwjo-8HG6s_MAhUFJJoKHUGfD68Q5hMIBTAA&url=http%3A%2F%2Femballages.arts-et-metiers.net%2Fimages%2Flogo-alimentaire.png&usg=AFQjCNEzioSTTT2hBxCHDf7On847HBz5nA&sig2=8O-X480gbTHKj5po0s3ong",
+			},
+		},
+	"name": "Offre de Noel",
+	"end_date":"20/12/16",
+	}];
+}
